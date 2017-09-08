e1d69d4489e7ea478a166f6628917471cdce7883
Gabriel Schippers <schippers_gabrel@mail.com>
2017-09-08 22:19:13 +0200

    Add AuthorType Class

---
+++ famous-quotes/src/AppBundle/Form/AuthorType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\FormBuilderInterface;
+use Symfony\Component\OptionResolver\OptionResolverInterface;
+
+class AuthorType extends AbstractType
+{
+    /**
+    * @param FormBuilderInterface $builder
+    * @param array $option
+    */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add ('auther')
+            ->add ('quote');
+    }
+
+    /**
+    * @param OptionResolverInterface $resolver
+    */
+    public function setDefaultOptions(OptionResolverInterface $resolver) 
+    {
+        $resolver->setDefault(array(
+            'data_class'=>'AppBundle/Entity/Author'
+        ));
+    }
+
+    /**
+    * @return string
+    */
+    public function getName(){
+        return 'famous_Quote_Auther';
+    }
+}
