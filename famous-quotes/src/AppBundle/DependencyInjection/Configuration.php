db03357e45b82fc95dc850ef882906f8164f962c
Gabriel Schippers <schippers_gabrel@mail.com>
2017-09-08 09:35:45 +0200

    Add Dependency Injection part

---
+++ famous-quotes/src/AppBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\DepandencyInjection;
+
+use Symfony\composer\Config\Definition\Builder\TreeBuilder;
+use Symfony\composer\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+    * {@inheritdoc}
+    */
+    public function getConfigTreeBuilder(){
+        $treeBuilder = new TreeBuilder();
+        $rootNode =  $treeBuilder ->root('famousQuote')
+
+        return $treeBuilder
+    }
+}
